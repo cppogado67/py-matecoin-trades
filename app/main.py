7d8b09b0c073213eb1b2e94b137f7399ac9dfc71
claro <c_ppogado67@yahoo.com>
2026-09-25 12:53:45 +0800

    33

--- app/main.py
+++ app/main.py
@@ -2,32 +2,32 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(filename):
-	with open(filename) as trades_file:
-		trades = json.load(trades_file)
+def calculate_profit(filename: str) -> None:
+    with open(filename) as trades_file:
+        trades = json.load(trades_file)
 
-	total_money = Decimal("0")
-	matecoin_account = Decimal("0")
+    total_money = Decimal("0")
+    matecoin_account = Decimal("0")
 
-	for trade in trades:
-		if trade["bought"] is not None:
-			bought = Decimal(str(trade["bought"]))
-			price = Decimal(str(trade["matecoin_price"]))
-			matecoin_account += bought
-			total_money -= bought * price
+    for trade in trades:
+        if trade["bought"] is not None:
+            bought = Decimal(str(trade["bought"]))
+            price = Decimal(str(trade["matecoin_price"]))
+            matecoin_account += bought
+            total_money -= bought * price
 
-		if trade["sold"] is not None:
-			sold = Decimal(str(trade["sold"]))
-			price = Decimal(str(trade["matecoin_price"]))
-			matecoin_account -= sold
-			total_money += sold * price
+        if trade["sold"] is not None:
+            sold = Decimal(str(trade["sold"]))
+            price = Decimal(str(trade["matecoin_price"]))
+            matecoin_account -= sold
+            total_money += sold * price
 
-	with open("profit.json", "w") as profit_file:
-		json.dump(
-			{
-				"earned_money": str(total_money),
-				"matecoin_account": str(matecoin_account),
-			},
-			profit_file,
-			indent=2,
-		)
+    with open("profit.json", "w") as profit_file:
+        json.dump(
+            {
+                "earned_money": str(total_money),
+                "matecoin_account": str(matecoin_account),
+            },
+            profit_file,
+            indent=2,
+        )
